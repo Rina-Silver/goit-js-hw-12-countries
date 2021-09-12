93ce45d6e9c37d01900cfbaadfbb6a6239f59c89
Унжакова Екатерина <katerina.unz@gmail.com>
2021-09-13 01:39:39 +0700

    Нотификация

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,10 @@
-export default function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => {
+const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+
+function fetchCountries(searchQuery) {
+    const url = `${BASE_URL}/${searchQuery}`;
+    return fetch(url).then(response => {
         return response.json();
     });
 }
+
+export default { fetchCountries };
--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,4 @@
-import fetchCountries from '../js/fetchCountries.js';
+import API from '../js/fetchCountries.js';
 
 import countryCardTemp from '../templates/country-card.hbs';
 import countriesListTemp from '../templates/countries-card-list.hbs';
@@ -8,31 +8,32 @@ import '@pnotify/core/dist/BrightTheme.css';
 const { info, error } = require('@pnotify/core');
 const debounce = require('lodash.debounce');
 
-// const data = {
-//     title: 'Title',
-// };
-// document.body.innerHTML = countryCardTemp(data);
-
 const refs = {
     searchForm: document.querySelector('.search-control'),
     cardContainer: document.querySelector('.js-card-container'),
 };
 
-refs.searchForm.addEventListener('input', onSearch);
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
     e.preventDefault();
-
+    //refs.cardContainer.innerHTML = '';
     const searchQuery = e.target.value;
 
-    fetchCountries(searchQuery)
-        .then(renderCountryCard)
-        .catch(error => console.error(error))
-        .finally(() => refs.searchForm.value === '');
+    API.fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
 }
 
 function renderCountryCard(country) {
     const markup = countryCardTemp(country[0]);
     refs.cardContainer.innerHTML = markup;
     //console.log(markup);
+}
+
+function onFetchError() {
+    error({
+        title: 'Invalid name of country entered',
+        text: 'Please enter correct query',
+        delay: 2000,
+        width: '500px',
+    });
 }
